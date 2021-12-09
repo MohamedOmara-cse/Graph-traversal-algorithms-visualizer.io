feaf093e6ac9427fc1c2fb118dadfe5112c8e0af
mostafa-hashhash <mostafasaeed483@gmail.com>
2021-12-09 20:20:20 +0200

    refactoring BFS and styling

--- map.js
+++ map.js
@@ -1,47 +1,28 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  let reachedDistination = false;
   let visited = new Set();
 
   const config = {
-    nodeActiveColor: "blue",
+    nodeActiveColor: "red",
     nodeInactiveColor: "yellow",
     lineActiveColor: "gray",
     lineInActiveColor: "black",
   };
 
   const roads = {
-    shibin: [
-      "elshohada",
-      "menouf",
-      "tala",
-      "elbagour",
-      "berket",
-      "ashmon",
-      "quesna",
-      "elsadat",
-    ],
+    shibin: ["shohada", "menouf", "tala", "elbagour", "berket", "ashmon", "quesna", "elsadat"],
     sirs: ["menouf", "elbagour"],
     menouf: ["sirs", "shibin", "elsadat"],
     elbagour: ["shibin", "sirs"],
     elsadat: ["shibin", "menouf"],
     ashmon: ["shibin"],
-    elshohada: ["shibin"],
+    shohada: ["shibin"],
     quesna: ["shibin"],
     tala: ["shibin"],
     berket: ["shibin"],
   };
 
   const mapRoads = {
-    shibin: [
-      "menouf",
-      "tala",
-      "elbagour",
-      "elshohada",
-      "elsadat",
-      "berket",
-      "ashmon",
-      "quesna",
-    ],
+    shibin: ["menouf", "tala", "elbagour", "shohada", "elsadat", "berket", "ashmon", "quesna"],
     sirs: ["menouf", "elbagour"],
     elsadat: ["menouf"],
   };
@@ -52,12 +33,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
     for (s in roads) {
       const x = document.getElementById(`${s}`).cx.animVal.value;
       const y = document.getElementById(`${s}`).cy.animVal.value;
-      const name = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "text"
-      );
-      name.setAttribute("x", `${x + 50}`);
-      name.setAttribute("y", `${y + 50}`);
+      const name = document.createElementNS("http://www.w3.org/2000/svg", "text");
+      name.setAttribute("x", `${x + 10}`);
+      name.setAttribute("y", `${y + 15}`);
       name.innerHTML = s;
       svg.appendChild(name);
     }
@@ -66,79 +44,58 @@ document.addEventListener("DOMContentLoaded", (event) => {
       const yStart = document.getElementById(`${i}`).cy.animVal.value;
 
       for (j in mapRoads[i]) {
-        const xDist = document.getElementById(`${mapRoads[i][j]}`).cx.animVal
-          .value;
-        const yDist = document.getElementById(`${mapRoads[i][j]}`).cy.animVal
-          .value;
-        const line = document.createElementNS(
-          "http://www.w3.org/2000/svg",
-          "line"
-        );
+        const xDist = document.getElementById(`${mapRoads[i][j]}`).cx.animVal.value;
+        const yDist = document.getElementById(`${mapRoads[i][j]}`).cy.animVal.value;
+        const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
 
         line.setAttribute("x1", `${xStart}`);
         line.setAttribute("y1", `${yStart}`);
         line.setAttribute("x2", `${xDist}`);
         line.setAttribute("y2", `${yDist}`);
         line.setAttribute("stroke", "black");
-        line.setAttribute("stroke-width", "5px");
+        line.setAttribute("stroke-width", "2px");
         svg.appendChild(line);
       }
     }
   })();
 
   function dfs(start, distination) {
-    console.log(start);
     visited.add(start);
-    delayBy(() => {
-      colorizeNode(start, config.nodeActiveColor);
-    }, 1);
+    delayBy(() => colorizeNode(start, undefined, config.nodeActiveColor), 1);
 
     if (start == distination) return true;
 
-    for (const child of roads[start]) {
-      if (visited.has(child)) continue;
-      else {
+    for (const child of roads[start])
+      if (!visited.has(child)) {
         visited.add(child);
         if (dfs(child, distination, visited)) return true;
       }
-    }
   }
 
-  function bfs(start, distination) {
+  function bfs(start, destination) {
     let queue = [];
 
     queue.push(start);
     visited.add(start);
-    colorizeNode(child, config.nodeActiveColor);
+    delayBy(() => colorizeNode(start, undefined, config.nodeActiveColor), 1);
 
     while (queue.length != 0) {
       let node = queue.shift();
+      if (start == destination) return true;
 
-      if (start == distination) {
-        reachedDistination = true;
-
-        break;
-      }
-      if (reachedDistination) {
-        break;
-      }
-      for (const child of roads[node]) {
+      for (const child of roads[node])
         if (!visited.has(child)) {
           visited.add(child);
-          queue.push(child);
-        }
+          delayBy(() => colorizeNode(child, undefined, config.nodeActiveColor), 1);
 
-        if (child == distination) {
-          reachedDistination = true;
-          break;
+          if (child == destination) return;
+          else queue.push(child);
         }
-      }
     }
-    return visited;
   }
 
-  function colorizeNode(id, color) {
-    document.querySelector(`#${id}`).style.fill = color;
+  function colorizeNode(id, prop = "fill", color) {
+    document.querySelector(`#${id}`).style[prop] = color;
   }
 
   function debounce() {
@@ -156,13 +113,10 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const delayBy = debounce();
 
   document.getElementById("form").addEventListener("submit", (e) => {
-    //Getting the form values on submit
     const start = document.getElementById("startCities").value;
     const distination = document.getElementById("distCities").value;
     const chosenAlgorithm = document.getElementById("algorithem").value;
     visited.clear();
-
-    reachedDistination = 0;
 
     switch (chosenAlgorithm) {
       case "dfs":
@@ -170,8 +124,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
         break;
       case "bfs":
         bfs(start, distination);
-        break;
-      default:
         break;
     }
 
