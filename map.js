08075ca72a9bd18a31f7c0353d8f3ac0fd1faf4c
mostafa-hashhash <mostafasaeed483@gmail.com>
2021-12-10 21:37:41 +0200

    adding the Astart Algorithm

--- map.js
+++ map.js
@@ -25,9 +25,71 @@ const data = {
 		sirs: ["menouf", "elbagour"],
 		elsadat: ["menouf"],
 	},
+
+	distances: {
+		shibin: { tala: 18, shohada: 16.5, menouf: 16.4, elbagour: 14.6, quesna: 18.7, berket: 14, ashmon: 42.2, elsadat: 61.4 },
+		sirs: { menouf: 4.2, elbagour: 7.1 },
+		menouf: { sirs: 4.2, shibin: 16.1, elsadat: 55.9 },
+		tala: { shibin: 18 },
+		berket: { shibin: 14 },
+		elbagour: { shibin: 14.9, sirs: 7.1 },
+		ashmon: { shibin: 42.2 },
+		quesna: { shibin: 18.1 },
+		elsadat: { shibin: 61.4, menouf: 54.5 },
+		shohada: { shibin: 17.5 },
+	},
+
+	Coordinates: {
+		shibin: [30.55492, 31.01239],
+		sirs: [30.4427159, 30.96665],
+		menouf: [30.4584, 30.93386],
+		tala: [30.68312, 30.95003],
+		berket: [30.62749, 31.0724],
+		elbagour: [30.4318, 31.03198],
+		ashmon: [30.30011, 30.9756],
+		quesna: [30.5676, 31.14956],
+		elsadat: [30.36281, 30.53315],
+		shohada: [30.5976, 30.89575],
+	}
+
 }
 
-const { roads, mapRoads, config } = data;
+Object.keys(data.Coordinates).forEach(node => {
+	data.Coordinates[node] = data.Coordinates[node].map(coordinate => Math.round((coordinate - 30) * 100));
+})
+
+class QElement {
+	constructor(node, priority) {
+		this.node = node;
+		this.priority = priority;
+	}
+}
+
+class PriorityQueue {
+
+	constructor() {
+		this.items = [];
+	}
+
+	enqueue(element, priority) {
+		var qElement = new QElement(element, priority);
+		var contain = false;
+
+		for (var i = 0; i < this.items.length; i++)
+			if (this.items[i].priority > qElement.priority) {
+				this.items.splice(i, 0, qElement);
+				contain = true;
+				break;
+			}
+
+		if (!contain) {
+			this.items.push(qElement);
+		}
+	}
+
+}
+
+const { roads, mapRoads, config, distances, Coordinates } = data;
 let visited = new Set();
 let timeout;
 
@@ -73,16 +135,12 @@ const Utils = {
 	},
 
 	addLineBetween(start, distination) {
-		const startX = document.getElementById(`${start}`).cx.animVal.value;
-		const startY = document.getElementById(`${start}`).cy.animVal.value;
-		const distinationX = document.getElementById(`${distination}`).cx.animVal.value;
-		const distinationY = document.getElementById(`${distination}`).cy.animVal.value;
 
 		const lineConfig = {
-			x1: startX,
-			x2: distinationX,
-			y1: startY,
-			y2: distinationY,
+			x1: document.getElementById(`${start}`).cx.animVal.value,
+			x2: document.getElementById(`${distination}`).cx.animVal.value,
+			y1: document.getElementById(`${start}`).cy.animVal.value,
+			y2: document.getElementById(`${distination}`).cy.animVal.value,
 			stroke: "black",
 			strokeWidth: "1px",
 		};
@@ -94,6 +152,14 @@ const Utils = {
 		});
 
 		svg.appendChild(line);
+	},
+
+	calculateHeuristic(node, destination) {
+
+		return Math.round(Math.sqrt(
+			Math.pow(Math.abs(Coordinates[node][0] - Coordinates[destination][0]), 2) +
+			Math.pow(Math.abs(Coordinates[node][1] - Coordinates[destination][1]), 2)
+		));
 	}
 }
 
@@ -123,6 +189,23 @@ const Traversals = {
 				if (!visited.has(child)) {
 					visited.add(child);
 					queue.push(child);
+				}
+		}
+	},
+
+	astar: function (start, destination) {
+		pQueue = new PriorityQueue();
+		pQueue.enqueue(start, distances[start][destination] + Utils.calculateHeuristic(start, destination));
+		while (pQueue.items.length) {
+			const { node, priority } = pQueue.items.shift();
+			visited.add(node);
+			delayBy(() => Utils.colorizeNode(node, config.nodeActiveColor), 1);
+			if (node == destination) return true;
+			for (const child of roads[node])
+				if (!visited.has(child)) {
+					visited.add(child);
+					pQueue.enqueue(child, distances[node][child] + Utils.calculateHeuristic(child, destination));
+					console.table(pQueue.items);
 				}
 		}
 	}
