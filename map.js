09b75af600765ada07f6ffc12e273852c2483a0d
mohamed <https://github.com/MohamedOmara-cse>
2021-12-25 21:21:45 +0200

    ui changes

--- map.js
+++ map.js
@@ -1,28 +1,7 @@
 let { userNodes, userNodesEdges, SVGNodesPositions, roads, mapRoads, config, distances, Coordinates } = data;
-let visited = new Set();
-let path = new Set();
-let currentTimeoutId;
-let previousTimeoutId = 0;
-let lineNodesNames = [];
-let roadSelection = [];
-const algoButtons = document.querySelector("#algorithmBtn");
-const messege = document.querySelector("#message");
 const { delayBy, resetCounter } = Utils.debounce();
-
 document.addEventListener("DOMContentLoaded", (e) => {
-
-	const resetButton = document.querySelector("#resetButton");
-
-	const algoButtonsContainer = document.querySelector("#algorithmBtn");
-
-	const generateButton = document.getElementById("generateGraph");
-	const saveButton = document.querySelector("#saveNodes");
-	const userInputNodesDiv = document.querySelector("#userInputNodeDiv");
-	let counterOfSelectedNode = 0;
-
 	Utils.corrdinatesProcess();
-	//Utils.randomPosition();
-
 	(function draw() {
 		Utils.addLineBetween(roads, SVGNodesPositions, svg);
 		Utils.addNodeCircleWithName(SVGNodesPositions, svg);
@@ -30,127 +9,38 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
 	Utils.flowMessage(counterOfSelectedNode);
 
-	svg.onclick = svg1Hnadler(e)
+	svg.onclick = Utils.selectStartAndDestination;
+
+	resetButton.onclick = Utils.resetPath;
+	algoButtonsContainer.onclick = Utils.startTraversingGraph;
 
 
-	function svg1Hnadler(e) {
-		if (e.target.tagName == "circle") {
-			let targetNode = e.target.classList.value;
-			targetNode = targetNode.split(" ")[0];
-			if (counterOfSelectedNode < 2) {
-				Utils.colorizeElement("node", targetNode, "select");
-				roadSelection[counterOfSelectedNode++] = targetNode;
-				Utils.flowMessage(counterOfSelectedNode);
-			}
-		}
-	};
+	generateButton.onclick = Utils.generateNewGragh;
 
-	resetButton.onclick = (e) => {
-		for (i in roadSelection) {
-			Utils.colorizeElement("node", roadSelection[i], "select");
-		}
-		roadSelection = [];
-		counterOfSelectedNode = 0;
-		algoButtonsContainer.classList.remove("deactive");
-		Utils.resetNodesPath();
-		visited.clear();
-		resetCounter();
-		isDone = false;
-		saveButton.classList.remove("deactiveSave");
-	};
-
-	algoButtonsContainer.onclick = (e) => {
-		if (e.target.tagName == "BUTTON") {
-			const targetAlgorithm = e.target.dataset.algo;
-			const start = roadSelection[0];
-			const destination = roadSelection[1];
-
-			if (start && destination && targetAlgorithm) {
-				algoButtonsContainer.classList.toggle("deactive");
-				if (svg.style.display != "none") Traversals[targetAlgorithm](start, destination, roads);
-				else Traversals[targetAlgorithm](start, destination, userNodesEdges);
-			}
-		}
-	};
-
-	generateButton.onclick = () => {
-		svg.style.display = "none";
-		isDone = false;
-		userInputNodesDiv.style.display = "block";
-		svg2.innerHTML = "";
-		document.querySelector("#astarButton").style.display = "none";
-		userNodesEdges = {};
-		userNodes = {};
-		saveButton.classList.remove("deactiveSave");
-	};
-
-	let isDone = false;
 	Utils.displayLineSelectionIndicator(lineNodesNames.length);
-	let ratio = screen.width / (70 * (screen.width < 768 ? 1 : 2));
 
-	svg2.onclick = (e) => {
-
-		let { x: offsetX, y: offsetY } = svg2.getClientRects()[0];
-		let nodeRelativePositionX = (e.clientX - offsetX) / ratio;
-		let nodeRelativePositionY = (e.clientY - offsetY) / ratio;
-
-		if (e.target.tagName == "circle" || e.target.tagName == "text") {
-
-
-			const chosenNodeName = Utils.getIdentifierFromEventTarget(e.target);
-
-			if (!isDone) {
-
-				lineNodesNames.push(chosenNodeName);
-
-				Utils.displayLineSelectionIndicator(lineNodesNames.length);
-
-				if (lineNodesNames.length == 2) {
-					let [firstNode, secondNode] = lineNodesNames;
-
-					if (firstNode != secondNode) {
-						userNodesEdges[secondNode].add(firstNode);
-						userNodesEdges[firstNode].add(secondNode);
-
-						Utils.addLineBetween(
-							{ [firstNode]: [secondNode] },
-							userNodes,
-							svg2
-						);
-					}
-
-					lineNodesNames = [];
-					Utils.displayLineSelectionIndicator(lineNodesNames.length);
-				}
-			} else {
-				if (counterOfSelectedNode < 2) {
-					Utils.colorizeElement("node", chosenNodeName, "select");
-					roadSelection[counterOfSelectedNode++] = chosenNodeName;
-					Utils.flowMessage(counterOfSelectedNode);
-				}
-			}
-
-		} else if (!isDone && e.target.tagName != "text") {
-			let keyValue = Object.keys(userNodes).length;
-			userNodes[`node${keyValue}`] = { cx: nodeRelativePositionX, cy: nodeRelativePositionY };
-			userNodesEdges[`node${keyValue}`] = new Set();
-			Utils.addNodeCircleWithName({ [`node${keyValue}`]: userNodes[`node${keyValue}`] }, svg2);
-		}
-	};
-
-	saveButton.onclick = () => {
-		if (isDone != true) {
+	svg2.onclick = Utils.userInputGeneration;
+	saveButton.onclick = () => {	
 			isDone = true;
 			saveButton.classList.add("deactiveSave");
 			svg2.innerHTML = "";
 			Utils.addLineBetween(userNodesEdges, userNodes, svg2);
 			Utils.addNodeCircleWithName(userNodes, svg2);
+
+	};
+
+	svg2.ondblclick = (e) => {
+		if (e.target.tagName == "circle" || e.target.tagName == "text") {
+			console.log("dblclick");
+			const removedNodeName = Utils.getIdentifierFromEventTarget(e.target);
+
+			Utils.removeNode(removedNodeName);
 		}
 	};
 	e.preventDefault();
 });
 
-
+/*
 svg2.onmousedown = mouseEventsHandler
 svg2.onmouseup = mouseEventsHandler
 
@@ -177,3 +67,4 @@ function mouseEventsHandler(e) {
 		}
 	}
 }
+*/
--- utils.js
+++ utils.js
@@ -1,30 +1,142 @@
 let activeNodesSet = new Set();
+let visited = new Set();
+let path = new Set();
+let currentTimeoutId;
+let previousTimeoutId = 0;
+let lineNodesNames = [];
+let roadSelection = [];
+let userNodeNumber = 0;
+const algoButtons = document.querySelector("#algorithmBtn");
+const messege = document.querySelector("#message");
+let isDone = false;
+let ratio = screen.width / (70 * (screen.width < 768 ? 1 : 2));
+const algoButtonsContainer = document.querySelector("#algorithmBtn");
+const resetButton = document.querySelector("#resetButton");
+const generateButton = document.getElementById("generateGraph");
+const saveButton = document.querySelector("#saveNodes");
+const userInputNodesDiv = document.querySelector("#userInputNodeDiv");
+let counterOfSelectedNode = 0;
+
 const Utils = {
 	colorizeElement: function (type, className, target) {
 		const targetElement = document.querySelector(`.${className}`);
-		if (type == "node")
-			if (target == "select") targetElement.classList.toggle("selectNode");
-			else if (target == "active") {
-				activeNodesSet.add(className);
-				targetElement.classList.toggle("activeNode");
-			} else {
-				targetElement.classList.remove("selectNode");
-				targetElement.classList.remove("activeNode");
-			}
-		else {
-			if (target == "active") {
-				targetElement.classList.toggle("activeLine");
-				path.add(className);
-			} else targetElement.classList.toggle("activeLine");
+		const styleClass = target + type.charAt(0).toUpperCase() + type.slice(1);
+		if (target == "active" || target == "select") {
+			targetElement.classList.toggle(`${styleClass}`);
+			if (styleClass == "activeNode") activeNodesSet.add(className);
+			if (styleClass == "activeLine") path.add(className);
+		} else {
+			targetElement.classList.remove(`active${type.charAt(0).toUpperCase() + type.slice(1)}`);
 		}
 	},
+	userInputGeneration: function (e) {
+		let { x: offsetX, y: offsetY } = svg2.getClientRects()[0];
+		let nodeRelativePositionX = (e.clientX - offsetX) / ratio;
+		let nodeRelativePositionY = (e.clientY - offsetY) / ratio;
 
+		if (e.target.tagName == "circle" || e.target.tagName == "text") {
+			const chosenNodeName = Utils.getIdentifierFromEventTarget(e.target);
+			console.log(isDone);
+			if (!isDone) {
+				lineNodesNames.push(chosenNodeName);
+				if (lineNodesNames.length > 2) lineNodesNames = [];
+				Utils.displayLineSelectionIndicator(lineNodesNames.length);
+
+				if (lineNodesNames.length == 2) {
+					let [firstNode, secondNode] = lineNodesNames;
+
+					if (firstNode != secondNode) {
+						userNodesEdges[secondNode].add(firstNode);
+						userNodesEdges[firstNode].add(secondNode);
+
+						Utils.addLineBetween({ [firstNode]: [secondNode] }, userNodes, svg2);
+					}
+
+					lineNodesNames = [];
+					Utils.displayLineSelectionIndicator(lineNodesNames.length);
+				}
+			} else {
+				if (counterOfSelectedNode < 2) {
+					Utils.colorizeElement("node", chosenNodeName, "select");
+					roadSelection[counterOfSelectedNode++] = chosenNodeName;
+					Utils.flowMessage(counterOfSelectedNode);
+				}
+			}
+		} else if (!isDone && e.target.tagName != "text") {
+			userNodes[`node${userNodeNumber}`] = { cx: nodeRelativePositionX, cy: nodeRelativePositionY };
+			userNodesEdges[`node${userNodeNumber}`] = new Set();
+			Utils.addNodeCircleWithName({ [`node${userNodeNumber}`]: userNodes[`node${userNodeNumber}`] }, svg2);
+			userNodeNumber++;
+		}
+	},
+	selectStartAndDestination: function (e) {
+		if (e.target.tagName == "circle") {
+			console.log("pressed");
+			let targetNode = e.target.classList.value;
+			targetNode = targetNode.split(" ")[0];
+			if (counterOfSelectedNode < 2) {
+				Utils.colorizeElement("node", targetNode, "select");
+				roadSelection[counterOfSelectedNode++] = targetNode;
+				Utils.flowMessage(counterOfSelectedNode);
+			}
+		}
+	},
+	startTraversingGraph: function (e) {
+		if (e.target.tagName == "BUTTON") {
+			const targetAlgorithm = e.target.dataset.algo;
+			const start = roadSelection[0];
+			const destination = roadSelection[1];
+
+			if (start && destination && targetAlgorithm) {
+				algoButtonsContainer.classList.toggle("deactive");
+				if (svg.style.display != "none") Traversals[targetAlgorithm](start, destination, roads);
+				else Traversals[targetAlgorithm](start, destination, userNodesEdges);
+			}
+		}
+	},
+	resetPath: function () {
+		for (i in roadSelection) {
+			Utils.colorizeElement("node", roadSelection[i], "select");
+		}
+		roadSelection = [];
+		lineNodesNames = [];
+		counterOfSelectedNode = 0;
+		algoButtonsContainer.classList.remove("deactive");
+		Utils.resetNodesPath();
+		visited.clear();
+		resetCounter();
+		isDone = false;
+		saveButton.classList.remove("deactiveSave");
+	},
+
+	generateNewGragh: function () {
+		svg.style.display = "none";
+		isDone = false;
+		userInputNodesDiv.style.display = "block";
+		svg2.innerHTML = "";
+		document.querySelector("#astarButton").style.display = "none";
+		userNodesEdges = {};
+		userNodes = {};
+		userNodeNumber = 0;
+		saveButton.classList.remove("deactiveSave");
+	},
+	removeNode: function (removedNode) {
+		delete userNodes[removedNode];
+		userNodesEdges[removedNode].forEach((connectedNode) => {
+			userNodesEdges[connectedNode].forEach((child) => {
+				if (child == removedNode) userNodesEdges[connectedNode].delete(child);
+			});
+		});
+		delete userNodesEdges[removedNode];
+		svg2.innerHTML = "";
+		Utils.addLineBetween(userNodesEdges, userNodes, svg2);
+		Utils.addNodeCircleWithName(userNodes, svg2);
+	},
 	displayLineSelectionIndicator: function (n) {
 		edgeMessage.innerHTML = `${n} Edge Nodes selected`;
 	},
 
 	resetNodesPath: function () {
-		console.table(activeNodesSet, path);
 		for (; previousTimeoutId <= currentTimeoutId; previousTimeoutId++) clearTimeout(previousTimeoutId);
 		for (node of activeNodesSet) this.colorizeElement("node", node, "inactive");
 		for (line of path) this.colorizeElement("line", line);
@@ -73,17 +185,15 @@ const Utils = {
 				cy: circleData.cy * ratio,
 				cursor: "Pointer",
 			};
-			if (svg.id == "svg2") {
-				name.innerHTML = node.split("node")[1];
-				name.setAttribute("x", circleConfig.cx - 5 * (node.length - 4));
-				name.setAttribute("y", circleConfig.cy + 5);
-				name.style.fontWeight = "bold";
-				name.style.cursor = "pointer";
-			} else {
-				name.innerHTML = node;
-				name.setAttribute("x", circleConfig.cx + 25);
-				name.setAttribute("y", circleConfig.cy + -7);
-			}
+			const svgId = svg.id;
+			const nameConfig = {
+				svg: { x: circleConfig.cx + 25, y: circleConfig.cy - 7, inner: node },
+				svg2: { x: circleConfig.cx - 5 * (node.length - 4), y: circleConfig.cy + 5, inner: node.split("node")[1] },
+			};
+			name.setAttribute("x", nameConfig[svgId].x);
+			name.setAttribute("y", nameConfig[svgId].y);
+			name.innerHTML = nameConfig[svgId].inner;
+
 			Object.keys(circleConfig).forEach((attribute) => {
 				circle.setAttribute(attribute, circleConfig[attribute]);
 			});
@@ -122,16 +232,15 @@ const Utils = {
 		return Math.round(
 			Math.sqrt(
 				Math.pow(Math.abs(Coordinates[node][0] - Coordinates[destination][0]), 2) +
-				Math.pow(Math.abs(Coordinates[node][1] - Coordinates[destination][1]), 2)
+					Math.pow(Math.abs(Coordinates[node][1] - Coordinates[destination][1]), 2)
 			)
 		);
 	},
 
 	getIdentifierFromEventTarget(target) {
 		if (target.tagName == "circle") return target.classList.value.split(" ")[0];
-		else return 'node' + target.innerHTML;
-	}
-
+		else return "node" + target.innerHTML;
+	},
 
 	/*	randomPosition: () => {
 		console.log(SVGNodesPositions);
